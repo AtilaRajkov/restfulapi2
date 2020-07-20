109b3e336b9ab511aa5e80e2cde2e800400140ae
AtilaRajkov <atilar777@gmial.com>
2020-07-20 13:48:04 +0200

    1. Implemented the properties for Category, Product, Transaction and User models, and the relationships between the models. 2. Created the migrations for users, categories, products, transactions and the category_product pivot table.

--- app/User.php
+++ app/User.php
@@ -5,35 +5,62 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  const VERIFIED_USER = '1';
+  const UNVERIFIED_USER = '0';
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  const ADMIN_USER = 'true';
+  const REGULAR_USER = 'false';
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   * @var array
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'verified',
+    'verification_token',
+    'admin',
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   * @var array
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+    'verification_token',
+  ];
+
+  /**
+   * The attributes that should be cast to native types.
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  public function isVerified()
+  {
+    return $this->verified == User::VERIFIED_USER;
+  }
+
+  public function isAdmin()
+  {
+    return $this->admin == User::ADMIN_USER;
+  }
+
+  public static function generateVerificationCode()
+  {
+    return Str::random(40);
+  }
+
 }
