8e831641c983cc2317c5aea42fab4d0a3c1f27b7
AtilaRajkov <atilar777@gmial.com>
2020-07-30 16:36:31 +0200

    Implemented the index, show, store, update, and destroy methods for the CategoryController. And implemented the index and show methods for the ProductController and the TransactionController.

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -2,84 +2,96 @@
 
 namespace App\Http\Controllers\Category;
 
+use App\Category;
 use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 
 class CategoryController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $categories = Category::all();
+    //return response()->json(['data' => $categories], 200);
+    return $this->showAll($categories);
+  }
+
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $rules = [
+      'name' => 'required|string|min:2|max:191|unique:categories',
+      'description' => 'sometimes|string|min:2|max:1000'
+    ];
+
+    $data = request()->validate($rules);
+
+    $category = Category::create($data);
+
+    //return response()->json(['data' => $category], 201);
+    return $this->showOne($category, 201);
+  }
+
+
+  /**
+   * Display the specified resource.
+   *
+   * @param \App\Category $category
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Category $category)
+  {
+    //return response()->json(['data' => $category]);
+    return $this->showOne($category);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @param \App\Category $category
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Category $category)
+  {
+    $category->fill(request()->only([
+      'name',
+      'description'
+    ]));
+
+    //if (!$category->isDirty()) {
+    if ($category->isClean()) {
+      return $this->errorResponse(
+        'You need to use a different value to be able tu update',
+        422);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    $category->save();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    //return response()->json(['data' => $category], 200);
+    return $this->showOne($category);
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param \App\Category $category
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Category $category)
+  {
+    $category->delete();
+    //return response()->json(['data' => $category], 200);
+    return $this->showOne($category);
+  }
 }
--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -3,83 +3,34 @@
 namespace App\Http\Controllers\Transaction;
 
 use App\Http\Controllers\ApiController;
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $transactions = Transaction::all();
+    //return response()->json(['data' => $transactions], 200);
+    return $this->showAll($transactions);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param \App\Transaction $transaction
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Transaction $transaction)
+  {
+    //return response()->json(['data' => $transaction], 200);
+    return $this->showOne($transaction);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
